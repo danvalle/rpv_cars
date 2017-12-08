eee8a2b034319a634e41ed7138473aca88bb9371
walyssonBarbosa <walysson.vital@gmail.com>
2017-12-08 12:23:17 -0200

    Computing heatmap, OA and AA.

--- cria_heatmap.py
+++ cria_heatmap.py
@@ -1,108 +1,109 @@
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-matplotlib.use('Agg')
-
-filename = 'models/4096_random_forest'
-y_pred, y_true = pickle.load(open(filename + '.sav', 'rb'))
+#filename = 'models/4096_random_forest'
+#y_pred, y_true = pickle.load(open(filename + '.sav', 'rb'))
 
 # filename = 'late_fusion_predictions'				 # saved only predictions for late fusion so only
 # y_pred = pickle.load(open(filename + '.sav', 'rb')) # uncomment this two lines and keep the above lines uncommented
 
-labels = list(set(sorted(y_true)))
+#labels = list(set(sorted(y_true)))
 
-m = np.zeros((25, 25))
-classes_total = {}
-classes_total_notright = {}
-predicted_right = 0
-for i in range(len(y_pred)):
-    if y_pred[i] == y_true[i]:
-        predicted_right += 1
-        if y_true[i] not in classes_total:
-            classes_total[y_true[i]] = 1
-        else:
-            classes_total[y_true[i]] += 1
-        m[labels.index(y_pred[i]) - 1, labels.index(y_pred[i]) - 1] += 1
-    else:
-        m[labels.index(y_pred[i]) - 1, labels.index(y_true[i]) - 1] += 1
+#m = np.zeros((25, 25))
+#classes_total = {}
+#classes_total_notright = {}
+#predicted_right = 0
+#for i in range(len(y_pred)):
+#    if y_pred[i] == y_true[i]:
+#        predicted_right += 1
+#        if y_true[i] not in classes_total:
+#            classes_total[y_true[i]] = 1
+#        else:
+#            classes_total[y_true[i]] += 1
+#        m[labels.index(y_pred[i]) - 1, labels.index(y_pred[i]) - 1] += 1
+#    else:
+#        m[labels.index(y_pred[i]) - 1, labels.index(y_true[i]) - 1] += 1
 
-    if y_true[i] not in classes_total_notright:
-        classes_total_notright[y_true[i]] = 1
-    else:
-        classes_total_notright[y_true[i]] += 1
+#    if y_true[i] not in classes_total_notright:
+#        classes_total_notright[y_true[i]] = 1
+#    else:
+#        classes_total_notright[y_true[i]] += 1
 
-acc_total = 0
-for class_ in classes_total_notright:
-    if class_ in classes_total:
-        num = classes_total[class_]
-    else:
-        num = 0
-    acc_total += num / (1. * classes_total_notright[class_])
+#acc_total = 0
+#for class_ in classes_total_notright:
+#    if class_ in classes_total:
+#        num = classes_total[class_]
+#    else:
+#        num = 0
+#    acc_total += num / (1. * classes_total_notright[class_])
 
-print(filename)
-print('AA:', acc_total / 25.)
-print('OA:', predicted_right / 1021.)
+#print(filename)
+#print('AA:', acc_total / 25.)
+#print('OA:', predicted_right / 1021.)
 
-sum_rowsm = np.sum(m, axis=1)
-sum_rowsm[sum_rowsm == 0] = 1
-m = m / sum_rowsm[:, np.newaxis]
+#sum_rowsm = np.sum(m, axis=1)
+#sum_rowsm[sum_rowsm == 0] = 1
+#m = m / sum_rowsm[:, np.newaxis]
 
 # filename = 'input_example.csv'
-# data = np.loadtxt(filename, delimiter=';')
-data = m
+#data = np.loadtxt(filename, delimiter=';')
+def cria_map(matrix, filename):
+    data = matrix
 
-desc = range(1, 26)  # ['Earth', 'Moon', 'Jupiter', 'Mars', 'Pluto', 'Saturn']
-# 
-strategy_path = []
-# 
-# # Computing strategies
-# for s in stat:
-for d in desc:
-    strategy_path.append(d)
-    # strategy_path.append(s + ': ' + d)
+    desc = range(1, 26)  # ['Earth', 'Moon', 'Jupiter', 'Mars', 'Pluto', 'Saturn']
+    # 
+    strategy_path = []
+    # 
+    # # Computing strategies
+    # for s in stat:
+    for d in desc:
+        strategy_path.append(d)
+        # strategy_path.append(s + ': ' + d)
 
-##############################################################################
+    ##############################################################################
 
-#  Finishing Touches
-fig, ax = plt.subplots()
-# using the ax subplot object, we use the same
-# syntax as above, but it allows us a little
-# bit more advanced control
-# ax.pcolor(data,cmap=plt.cm.Reds,edgecolors='k')
-# ax.pcolor(data,cmap=plt.cm.Greens)
-# ax.pcolor(data,cmap=plt.cm.gnuplot)
-ax.set_xticks(np.arange(0, len(strategy_path)))
-ax.set_yticks(np.arange(0, len(strategy_path)))
+    #  Finishing Touches
+    fig, ax = plt.subplots()
+    # using the ax subplot object, we use the same
+    # syntax as above, but it allows us a little
+    # bit more advanced control
+    # ax.pcolor(data,cmap=plt.cm.Reds,edgecolors='k')
+    # ax.pcolor(data,cmap=plt.cm.Greens)
+    # ax.pcolor(data,cmap=plt.cm.gnuplot)
+    ax.set_xticks(np.arange(0, len(strategy_path)))
+    ax.set_yticks(np.arange(0, len(strategy_path)))
 
-# cmap = plt.get_cmap('BlueRed2')
-plt.imshow(data, cmap=plt.cm.gnuplot, interpolation='nearest')
-# plt.imshow(data, cmap=plt.cm.gnuplot)
-# plt.clim(-0.05,0.25)
-plt.colorbar()
+    # cmap = plt.get_cmap('BlueRed2')
+    plt.imshow(data, cmap=plt.cm.gnuplot, interpolation='nearest')
+    # plt.imshow(data, cmap=plt.cm.gnuplot)
+    # plt.clim(-0.05,0.25)
+    plt.colorbar()
 
-# Here we put the x-axis tick labels
-# on the top of the plot.  The y-axis
-# command is redundant, but inocuous.
-ax.xaxis.tick_top()
-ax.yaxis.tick_left()
-# similar syntax as previous examples
-ax.set_xticklabels(strategy_path, minor=False, fontsize=12, rotation=90)
-ax.set_yticklabels(strategy_path, minor=False, fontsize=12)
+    # Here we put the x-axis tick labels
+    # on the top of the plot.  The y-axis
+    # command is redundant, but inocuous.
+    ax.xaxis.tick_top()
+    ax.yaxis.tick_left()
+    # similar syntax as previous examples
+    ax.set_xticklabels(strategy_path, minor=False, fontsize=12, rotation=90)
+    ax.set_yticklabels(strategy_path, minor=False, fontsize=12)
 
-# Here we use a text command instead of the title
-# to avoid collision between the x-axis tick labels
-# and the normal title position
-# plt.text(0.5,1.08,'Main Plot Title',
-#         fontsize=25,
-#         horizontalalignment='center',
-#         transform=ax.transAxes
-#         ) 
+    # Here we use a text command instead of the title
+    # to avoid collision between the x-axis tick labels
+    # and the normal title position
+    # plt.text(0.5,1.08,'Main Plot Title',
+    #         fontsize=25,
+    #         horizontalalignment='center',
+    #         transform=ax.transAxes
+    #         ) 
 
-# standard axis elements
-# plt.ylabel('Y Axis Label',fontsize=10)
-# plt.xlabel('X Axis Label',fontsize=10)
+    # standard axis elements
+    # plt.ylabel('Y Axis Label',fontsize=10)
+    # plt.xlabel('X Axis Label',fontsize=10)
 
-plt.savefig(filename + '.pdf', bbox_inches='tight')
-plt.show()
+    plt.savefig(filename + '.png', bbox_inches='tight')
+    plt.savefig(filename + '.pdf', bbox_inches='tight')
+    #plt.show()
--- svm_grid_search.py
+++ svm_grid_search.py
@@ -2,17 +2,21 @@ import numpy as np
 import pickle
 import sys
 
-from sklearn.metrics import classification_report, confusion_matrix
+from cria_heatmap import cria_map
+from sklearn.metrics import average_precision_score, accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import scale
 from sklearn.svm import SVC
 from utils import split_dataset
 
+# usage: python svm_grid_search.py feature
 feature = sys.argv[1]
 X_path = feature + '_features.h5'
 
 # Loading the cars dataset features
 test_size = 0.3
 cars_train_X, cars_test_X, cars_train_y, cars_test_y = split_dataset(X_path, feature, test_size)
+
 cars_train_X = np.asarray(cars_train_X).reshape(cars_train_X.shape[0], np.prod(cars_train_X.shape[1:]))
 cars_test_X = np.asarray(cars_test_X).reshape(cars_test_X.shape[0], np.prod(cars_test_X.shape[1:]))
 
@@ -20,38 +24,41 @@ cars_test_X = np.asarray(cars_test_X).reshape(cars_test_X.shape[0], np.prod(cars
 tuned_parameters = [{'gamma': [1e-3, 1e-4, 1e-5],
                      'C': [1, 10, 100, 1000]}]
 
-score = 'accuracy'  # , 'precision', 'recall']
-
-print("# Tuning hyper-parameters for %s" % score)
+print("# Tuning hyper-parameters for accuracy")
 print()
 
-clf = GridSearchCV(SVC(kernel='rbf'), tuned_parameters, cv=5,
-                   scoring=score, n_jobs=8)  # '%s_macro' % score)
+clf = GridSearchCV(SVC(kernel='rbf', cache_size=1024), tuned_parameters, cv=5,
+                   scoring='accuracy', n_jobs=-1)
 clf.fit(cars_train_X, cars_train_y)
 
 print("Best parameters set found on development set:")
 print()
 print(clf.best_params_)
-print()
-print("Grid scores on development set:")
-print()
-means = clf.cv_results_['mean_test_score']
-stds = clf.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-    print("%0.3f (+/-%0.03f) for %r"
-          % (mean, std * 2, params))
-print()
+# print()
+# print("Grid scores on development set:")
+# print()
+# means = clf.cv_results_['mean_test_score']
+# stds = clf.cv_results_['std_test_score']
+# for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+#     print("%0.3f (+/-%0.03f) for %r"
+#           % (mean, std * 2, params))
+# print()
 
-print("Detailed classification report:")
-print()
-print("The model is trained on the full development set.")
-print("The scores are computed on the full evaluation set.")
-print()
-y_true, y_pred = cars_test_y, clf.predict(cars_test_X)
-print(classification_report(y_true, y_pred))
-print()
+# print("Detailed classification report:")
+# print()
+# print("The model is trained on the full development set.")
+# print("The scores are computed on the full evaluation set.")
+# print()
+# y_true, y_pred = cars_test_y, clf.predict(cars_test_X)
+# print(classification_report(y_true, y_pred))
+# print()
 
-print confusion_matrix(y_true, y_pred)
+conf_matrix = 1. * confusion_matrix(y_true, y_pred)
+conf_matrix /= np.sum(conf_matrix, axis=1)
+cria_map(conf_matrix, feature)
+
+print "OA:", accuracy_score(y_true, y_pred)
+print "AA:", np.trace(conf_matrix) / (1. * conf_matrix.shape[0])
 
 # save model for later use in plotting the heatmap
 filename = feature + '_model.sav'
